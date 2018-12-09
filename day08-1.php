6d8c7a5b092789aee24ce3de2d008d3aa1eec1aa
Reed Thayer <digitalfeonix@gmail.com>
2018-12-08 19:27:14 -0800

    Add day 8 solutions

---
+++ day08-1.php
@@ -0,0 +1,34 @@
+<?php
+
+$input = file_get_contents('./inputs/input08-1.txt');
+#$input = '2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2';
+$input = trim($input);
+
+$inputs = explode(" ", $input);
+
+// the first two numbers are children node count and meta data count
+// child nodes come before the meta data
+
+function get_nodes(&$in)
+{
+    $ret = 0;
+
+    $cc = array_shift($in);
+    $mc = array_shift($in);
+
+    for ($c = 0; $c < $cc; $c++)
+    {
+        $ret += get_nodes($in);
+    }
+
+    for ($m = 0; $m < $mc; $m++)
+    {
+        $ret += array_shift($in);
+    }
+
+    return $ret;
+}
+
+$sum = get_nodes($inputs);
+
+echo 'Part 1: ',$sum,"\n";
